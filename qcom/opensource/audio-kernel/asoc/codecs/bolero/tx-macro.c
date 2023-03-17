2917967b58c8775c038d0e8ad4f0ad355f53c9a3
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-17 18:58:24 +0600

    audio: bolero: queue work on power efficient freezable wq
    Change-Id: Ibfd36f8579cce2ecb45c0b545d226486f4e9e24a
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- qcom/opensource/audio-kernel/asoc/codecs/bolero/tx-macro.c
+++ qcom/opensource/audio-kernel/asoc/codecs/bolero/tx-macro.c
@@ -1134,7 +1134,7 @@ static int tx_macro_enable_dec(struct snd_soc_dapm_widget *w,
 		}
 		#endif /* OPLUS_ARCH_EXTENDS */
 		/* schedule work queue to Remove Mute */
-		queue_delayed_work(system_freezable_wq,
+		queue_delayed_work(system_freezable_power_efficient_wq,
 				   &tx_priv->tx_mute_dwork[decimator].dwork,
 				   #ifndef OPLUS_ARCH_EXTENDS
 				   msecs_to_jiffies(tx_unmute_delay));
@@ -1143,7 +1143,7 @@ static int tx_macro_enable_dec(struct snd_soc_dapm_widget *w,
 				   #endif /* OPLUS_ARCH_EXTENDS */
 		if (tx_priv->tx_hpf_work[decimator].hpf_cut_off_freq !=
 							CF_MIN_3DB_150HZ) {
-			queue_delayed_work(system_freezable_wq,
+			queue_delayed_work(system_freezable_power_efficient_wq,
 				&tx_priv->tx_hpf_work[decimator].dwork,
 				msecs_to_jiffies(hpf_delay));
 			snd_soc_component_update_bits(component,
